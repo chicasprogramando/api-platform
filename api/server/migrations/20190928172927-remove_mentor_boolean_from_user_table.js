8b80cd861e2993957718fabea0e13cd4102b7340
elstr <eleonora.lester@gmail.com>
2019-09-28 14:51:14 -0300

    feat: remove mentor boolean from user table migration

---
+++ api/server/migrations/20190928172927-remove_mentor_boolean_from_user_table.js
@@ -0,0 +1,26 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    // logic for reverting the changes
+    return queryInterface.removeColumn('User', "is_mentor");
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return queryInterface.addColumn("User", "is_mentor", Sequelize.BOOLEAN);
+  }
+};
