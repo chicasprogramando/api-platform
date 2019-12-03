358a12a2aa45abbbda58e13da72a9ee6d1604e8c
Eleonora Lester <eleonora.lester@gmail.com>
2019-12-03 13:10:36 -0300

    Revert "feature: Adds profile structure"

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ const config = require("dotenv");
 const express = require("express");
 const bodyParser = require("body-parser");
 const userRoutes = require("./server/routes/UserRoutes");
-const profileRoutes = require("./server/routes/ProfileRoutes");
 
 
 const cors = require('cors')
@@ -19,7 +18,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const port = process.env.PORT || 8000;
 
 app.use("/api/user", userRoutes);
-app.use("/api/profile", profileRoutes);
 
 // when a random route is inputed
 app.get("*", (req, res) =>
