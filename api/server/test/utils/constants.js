c0d95f702938f3c46193eebbf97c2f25abee615a
elstr <eleonora.lester@gmail.com>
2019-12-09 19:49:36 -0300

    feat: Adds more constants for testing

--- api/server/test/utils/constants.js
+++ api/server/test/utils/constants.js
@@ -3,6 +3,12 @@ const MOCKS = {
     user_name: "jennydoe",
     auth_sub: "google-oauth2|999999999999999999999",
     email: "jennydoe@gmail.com"
+  },
+  PROFILE: {
+    image_path: "https://avatars3.githubusercontent.com/u/3179348?s=460&v=4",
+    linkedin: "https://www.linkedin.com/in/eleonora-lester-7a432022",
+    github: "elstr",
+    twitter: "http://www.twitter.com/lele_lester"
   }
 };
 
@@ -17,10 +23,26 @@ const PROPS = {
     "ProfileId",
     "updatedAt",
     "createdAt"
+  ],
+  PROFILE: [
+    "id",
+    "UserId",
+    "github",
+    "twitter",
+    "linkedin",
+    "image_path",
+    "createdAt",
+    "updatedAt"
   ]
 };
 
+const FAKE_ID = {
+  USER:  "5e016e8e-1804-4c35-abdd-9c0427989999",
+  PROFILE: "da775875-5907-4f9e-a2bf-be9727c21bbf"
+}
+
 module.exports = {
   MOCKS,
-  PROPS
+  PROPS,
+  FAKE_ID,
 };
