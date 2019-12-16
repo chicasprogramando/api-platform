a6139fe073d637dafead1ea1bf88482ef8d7610b
elstr <eleonora.lester@gmail.com>
2019-12-16 15:55:16 -0300

    feat: Adds speciality model

---
+++ api/server/models/Specialty.js
@@ -0,0 +1,24 @@
+"use strict";
+const uuid = require("uuid/v4");
+
+module.exports = (sequelize, DataTypes) => {
+  const Specialty = sequelize.define(
+    "Specialty",
+    {
+      id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: uuid()
+      },
+      description: DataTypes.STRING
+    },
+    {}
+  );
+
+  Specialty.associate = models => {
+    Specialty.belongsToMany(models.Profile, { as: 'profile', through: 'profile_speciality', foreignKey: 'SpecialtyId',  otherKey: 'ProfileId' })
+  };
+
+  return Profile;
+};
