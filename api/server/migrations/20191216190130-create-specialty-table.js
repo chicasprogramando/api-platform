5f4f584c95f5a72942095a28ce2a7e56f38c2192
elstr <eleonora.lester@gmail.com>
2019-12-16 16:03:07 -0300

    feat: Create specialty table migration

---
+++ api/server/migrations/20191216190130-create-specialty-table.js
@@ -0,0 +1,29 @@
+const uuid = require("uuid/v4");
+("use strict");
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Specialty", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: uuid()
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("Specialty");
+  }
+};
