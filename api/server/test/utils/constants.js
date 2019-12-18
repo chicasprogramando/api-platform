1542ffee1f11f9b05f8174e395991f84cb6474e0
elstr <eleonora.lester@gmail.com>
2019-12-18 13:33:25 -0300

    feat: Adds specialties mock for testing

--- api/server/test/utils/constants.js
+++ api/server/test/utils/constants.js
@@ -8,8 +8,13 @@ const MOCKS = {
     image_path: "https://avatars3.githubusercontent.com/u/3179348?s=460&v=4",
     linkedin: "https://www.linkedin.com/in/eleonora-lester-7a432022",
     github: "elstr",
-    twitter: "http://www.twitter.com/lele_lester"
-  }
+    twitter: "http://www.twitter.com/lele_lester",
+  },
+  SPECIALTIES: [
+    { id: "d0cc0eea-6331-4c40-a75d-6a00e6911943", description: "Front End" },
+    { id: "3acc04bb-be14-4d38-be4a-56709805b461", description: "Back End" },
+    { id: "2ff867dc-3465-4af3-bc27-5a74d177b31a", description: "QA" }
+  ]
 };
 
 const PROPS = {
@@ -37,12 +42,12 @@ const PROPS = {
 };
 
 const FAKE_ID = {
-  USER:  "5e016e8e-1804-4c35-abdd-9c0427989999",
+  USER: "5e016e8e-1804-4c35-abdd-9c0427989999",
   PROFILE: "da775875-5907-4f9e-a2bf-be9727c21bbf"
-}
+};
 
 module.exports = {
   MOCKS,
   PROPS,
-  FAKE_ID,
+  FAKE_ID
 };
