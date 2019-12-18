53df28efb086f477612e58bdd551c8c634111702
elstr <eleonora.lester@gmail.com>
2019-12-18 12:27:54 -0300

    feat: Allow multiple .env files
    Now you can have a .env.testing file or whatever specific configs you need using .env.*

--- api/server/config/config.js
+++ api/server/config/config.js
@@ -1,5 +1,8 @@
-const env = require("dotenv");
-env.config();
+// const env = require("dotenv");
+const path = require('path'); 
+const env = require('dotenv');
+
+env.config({ path: path.join(__dirname, `../../../.env.${process.env.NODE_ENV}`)});
 
 const common = {
   username: process.env.DB_USER,
@@ -14,7 +17,11 @@ module.exports = {
     ...common
   },
   testing: {
-    ...common
+    username: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    dialect: "postgres"
   },
   production: {
     use_env_variable: "DATABASE_URL",
