af1fa07f7b64ff5582e1f603fb06d3f75dfc16ac
elstr <eleonora.lester@gmail.com>
2019-12-20 18:58:14 -0300

    feat: Adds constants and helpers for testing

--- api/server/test/utils/constants.js
+++ api/server/test/utils/constants.js
@@ -8,12 +8,18 @@ const MOCKS = {
     image_path: "https://avatars3.githubusercontent.com/u/3179348?s=460&v=4",
     linkedin: "https://www.linkedin.com/in/eleonora-lester-7a432022",
     github: "elstr",
-    twitter: "http://www.twitter.com/lele_lester",
+    twitter: "http://www.twitter.com/lele_lester"
   },
   SPECIALTIES: [
     { id: "d0cc0eea-6331-4c40-a75d-6a00e6911943", description: "Front End" },
     { id: "3acc04bb-be14-4d38-be4a-56709805b461", description: "Back End" },
     { id: "2ff867dc-3465-4af3-bc27-5a74d177b31a", description: "QA" }
+  ],
+  SKILLS: [
+    { id: "c7725624-6fb3-4a7f-9e6b-659d2a54592e", description: "Redis" },
+    { id: "fb7456a4-e25c-4e21-96e7-39ebd22cc02d", description: "Redshift" },
+    { id: "eef36f53-acf8-416a-95e6-58b3eb9f653d", description: "Redux.js" },
+    { id: "afb62a5e-8527-4cb8-966e-78edbdc94aac", description: "React.js" }
   ]
 };
 
@@ -39,18 +45,15 @@ const PROPS = {
     "createdAt",
     "updatedAt"
   ],
-  SPECIALTY: [
-    "id",
-    "description",
-    "createdAt",
-    "updatedAt"
-  ]
+  SPECIALTY: ["id", "description", "createdAt", "updatedAt"],
+  SKILL: ["id", "description", "createdAt", "updatedAt"]
 };
 
 const FAKE_ID = {
   USER: "5e016e8e-1804-4c35-abdd-9c0427989999",
   PROFILE: "da775875-5907-4f9e-a2bf-be9727c21bbf",
-  SPECIALTY: "d0cc0eea-6331-4c40-a75d-6a00e9999999"
+  SPECIALTY: "d0cc0eea-6331-4c40-a75d-6a00e9999999",
+  SKILL: "fb7456a4-e25c-4e21-96e7-39ebd22cc99d"
 };
 
 module.exports = {
