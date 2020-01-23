d7dcd391b2ad4dfae418bd2199e00575e07df434
Inventrixx <inveniahadouken@gmail.com>
2020-01-23 16:54:24 -0300

    feat: added mailer route

--- api/index.js
+++ api/index.js
@@ -1,18 +1,18 @@
 const config = require("dotenv");
-const cors = require('cors')
+const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
 const userRoutes = require("./server/routes/UserRoutes");
 const profileRoutes = require("./server/routes/ProfileRoutes");
 const specialtyRoutes = require("./server/routes/SpecialtyRoutes");
 const skillRoutes = require("./server/routes/SkillRoutes");
-
+const mailerRoutes = require("./server/routes/MailerRoutes");
 
 config.config();
 
 const app = express();
 
-app.use(cors({origin: 'http://localhost:8080',}))
+app.use(cors({ origin: "http://localhost:8080" }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,6 +23,7 @@ app.use("/api/user", userRoutes);
 app.use("/api/profile", profileRoutes);
 app.use("/api/specialty", specialtyRoutes);
 app.use("/api/skill", skillRoutes);
+app.use("/api/mailer", mailerRoutes);
 
 // when a random route is inputed
 app.get("*", (req, res) =>
@@ -35,4 +36,4 @@ app.listen(port, () => {
   console.log(`Server is running on PORT ${port}`);
 });
 
-module.exports = app
+module.exports = app;
