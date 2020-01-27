30c89b3aa2ff9f83203380767e692bb4224d5a5a
elstr <eleonora.lester@gmail.com>
2020-01-27 11:52:21 -0300

    fix: Change get route param
    - Now we use ID and not aut_sub to get users

--- api/server/routes/UserRoutes.js
+++ api/server/routes/UserRoutes.js
@@ -4,7 +4,7 @@ const UserController = require("../controllers/UserController");
 const router = express.Router();
 
 router.post("/", UserController.addUser);
-router.get("/:auth_sub", UserController.getUser);
+router.get("/:id", UserController.getUser);
 router.get("/", UserController.getAllUsers);
 router.put("/:id", UserController.updateUser);
 router.delete("/:id", UserController.deleteUser);
