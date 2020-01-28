201d2a7dc24ff77320711fc2ce404afb026ca01d
elstr <eleonora.lester@gmail.com>
2020-01-28 11:49:29 -0300

    feat: Check token on user update

--- api/server/routes/UserRoutes.js
+++ api/server/routes/UserRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const {checkJwt} = require("../middlewares/secure")
 const UserController = require("../controllers/UserController");
 
 const router = express.Router();
@@ -6,7 +7,7 @@ const router = express.Router();
 router.post("/", UserController.addUser);
 router.get("/:id", UserController.getUser);
 router.get("/", UserController.getAllUsers);
-router.put("/:id", UserController.updateUser);
+router.put("/:id", checkJwt, UserController.updateUser);
 router.delete("/:id", UserController.deleteUser);
 
 module.exports = router;
